dcad3864c9e9d929f53473ddc96c8975a36c5930
anandsreenivasan <anand.sreenivasan@hotmail.com>
2024-01-01 08:34:16 -0500

    Flogle web welcome pag change flogile to flogiletechnologies

--- flogile_v2.1/src/components/misc/Layouts.js
+++
@@ -1,7 +0,0 @@
-import tw from "twin.macro";
-
-export const Container = tw.div` max-w-full  relative`;
-export const ContentWithPaddingXl= tw.div`max-w-full mx-auto py-0 lg:py-0`;
-export const ContentWithPaddingLg= tw.div`max-w-screen-lg mx-auto py-2 lg:py-10`;
-export const ContentWithVerticalPadding = tw.div`py-20 lg:py-24`;
-export const Content2Xl= tw.div`max-w-screen-2xl mx-auto`;
--- flogile_v2.1/src/components/misc/Typography.js
+++
@@ -1,2 +0,0 @@
-import tw from "twin.macro";
-export const SectionDescription = tw.p`mt-4 text-sm md:text-base lg:text-lg  leading-relaxed text-secondary-300 max-w-2xl`;
